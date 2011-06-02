f107c8be7e193566fea46fa24accfa873f5b70b6
Barry Arthur <barry.arthur@gmail.com>
2011-06-02 20:18:26 +0800

    XDG_DATA_HOME created; added article_queue.py from https://github.com/TaylanUB/misc/blob/c8207f8125d76e87dd7d4968a2a7a53a9a401101/home/tub/etc/uzbl/scripts/article_queue.py

---
+++ .local/share/uzbl/scripts/article_queue.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+import os
+from sys import argv
+import json
+import socket
+from datetime import datetime
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime):
+            values = {}
+            dt = {'__datetime__' : values}
+            values['year'] = o.year
+            values['month'] = o.month
+            values['day'] = o.day
+            values['hour'] = o.hour
+            values['minute'] = o.minute
+            values['second'] = o.second
+            values['microsecond'] = o.microsecond
+            values['tzinfo'] = o.tzinfo
+            values['__string_repr__'] = o.isoformat()
+            return dt
+        else:
+            return json.JSONEncoder.default(self, o)
+
+def object_decoder(d):
+    if '__datetime__' in d:
+        o =  d['__datetime__']
+        dt = datetime(o['year'], o['month'], o['day'], o['hour'], o['minute'], o['second'], o['microsecond'], o['tzinfo'])
+        return dt
+    return d
+
+def fetch_queue():
+    filename = os.environ['XDG_CONFIG_HOME'] + '/uzbl/article_queue'
+    if os.path.exists(filename) and os.path.getsize(filename) > 0:
+        qfile = open(filename)
+        return json.load(qfile, object_hook=object_decoder)
+    else:
+        return []
+
+def persist_queue(queue):
+    filename = os.environ['XDG_CONFIG_HOME'] + '/uzbl/article_queue'
+    qfile = open(filename, 'w')
+    json.dump(queue, qfile, cls=JSONEncoder)
+
+def create_url():
+    return {'url' : os.environ['UZBL_URI'], 'title' : os.environ['UZBL_TITLE'], 'timestamp' : datetime.now()}
+
+def push(queue):
+    queue.insert(0, create_url())
+
+def pop(queue, i):
+    try: return queue.pop(i-1)
+    except IndexError:
+        fifo = open(os.environ['UZBL_FIFO'], "a")
+        fifo.write('js alert("'+str(i)+': Queue doesn\'t have that many entries");\n')
+        fifo.close()
+
+def shift(queue):
+    if len(queue) > 0:
+        return queue.pop(-1)
+    fifo = open(os.environ['UZBL_FIFO'], "a")
+    fifo.write("js alert('Queue empty');\n")
+    fifo.close()
+
+def append(queue):
+    queue.append(create_url())
+
+def forward(queue):
+    append(queue)
+    return pop(queue, -1)
+
+def back(queue):
+    push(queue)
+    return shift(queue)
+
+def list_queue(queue):
+    qs = ""
+    first = True
+    for item in queue:
+        if not first:
+            qs += "\\n"
+        first = False
+        qs += item['title']
+    qs = qs.replace('"', '"')
+    qs = qs.replace('(', '[')
+    qs = qs.replace(')', ']')
+    command = "js alert(\"%s\");\n" % qs
+    print command
+    fifo = open(os.environ['UZBL_FIFO'], "a")
+    fifo.write(command.encode('ascii', 'replace'))
+    fifo.close()
+
+def write_fifo(url):
+    fifo = open(os.environ['UZBL_FIFO'], "a")
+    if url:
+        fifo.write("uri " + url['url'] + "\n")
+    else:
+        fifo.write("exit\n")
+    fifo.close()
+
+def main():
+    usage = "usage: "+argv[0]+" push|pop [x]|append|shift|forward|back|list"
+    act=argv[1]
+    queue = fetch_queue()
+    url = None
+    if act == "push":
+        url = push(queue)
+    elif act == "pop":
+        try: url = pop(queue, int(argv[2]))
+        except IndexError: url = pop(queue, 0)
+    elif act == "shift":
+        url = shift(queue)
+    elif act == "append":
+        url = append(queue)
+    elif act == "list":
+        list_queue(queue)
+        return
+    elif act == "forward":
+        url = forward(queue)
+    elif act == "back":
+        url = back(queue)
+    else:
+        print usage
+        exit(1)
+    persist_queue(queue)
+    write_fifo(url)
+
+if __name__ == "__main__":
+    main()
