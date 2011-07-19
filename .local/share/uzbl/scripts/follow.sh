c7ae90cc65232ec62787402d12bed7369963bf53
Barry Arthur <barry.arthur@gmail.com>
2011-07-19 11:25:27 +0800

    Updated uzbl configs for Commit: 91da964; added customised follow.sh script.

---
+++ .local/share/uzbl/scripts/follow.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# This scripts acts on the return value of followLinks in follow.js 
+
+result=$1
+shift
+
+uriaction=$1
+shift
+
+case "$result" in
+    XXXEMIT_FORM_ACTIVEXXX)
+        # a form element was selected
+        printf 'event KEYCMD_CLEAR\n' > "$UZBL_FIFO"
+        ;;
+    XXXRESET_MODEXXX)
+        # a link was selected, reset uzbl's input mode
+        printf 'set mode=\nevent KEYCMD_CLEAR\n' > "$UZBL_FIFO"
+        ;;
+    XXXRETURNED_URIXXX*)
+        uri=${result#XXXRETURNED_URIXXX}
+
+        case "$uriaction" in
+            set)
+                printf 'uri '"$uri"'\n' | sed -e 's/@/\\@/' > "$UZBL_FIFO"
+                ;;
+            clipboard)
+                printf "$uri" | xclip
+                ;;
+            *)
+                printf "$uriaction"' '"$uri"'\n' | sed -e 's/@/\\@/' > "$UZBL_FIFO"
+                ;;
+        esac
+        printf 'set mode=\nevent KEYCMD_CLEAR\n' > "$UZBL_FIFO"
+esac
